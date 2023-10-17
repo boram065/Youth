a3a10e1106c0ae6980f45f681867555f747ff9af
boram065 <boram4784@gmail.com>
2023-10-17 15:40:22 +0900

    navbar 완성

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,8 @@
-function sayHello() {
-    alert("안녕!!");
-}
 const navToggleI = nav_toggle.getElementsByTagName("i")[0]; //nav_toggle 요소안에 찾기
 const navListUl = document.getElementsByClassName("nav-list")[0];
+const firstNavItem = document.querySelector('.nav-item:first-child');
 nav_toggle.onclick = () => {
+    firstNavItem.style.display = 'none';
     navToggleI.classList.toggle("bi-list");
     navToggleI.classList.toggle("bi-x-lg");
     navListUl.classList.toggle("show-menu");
