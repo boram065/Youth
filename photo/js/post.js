e93748cc0bf1b93756f59ee6d1b3b6b6471ac832
haewon <haewon1106@gmail.com>
2023-10-25 20:43:54 +0900

    이미지 업로드 기능 구현

--- photo/js/post.js
+++ photo/js/post.js
@@ -1,22 +1,36 @@
-var submit = document.getElementById('submitButton');
-submit.onclick = showImage;     // Submit 버튼 클릭시 이미지 보여주기
+// var submit = document.getElementById('submitButton');
+// submit.onclick = showImage;     // Submit 버튼 클릭시 이미지 보여주기
 
-function showImage() {
-    var newImage = document.getElementById('image-show').lastElementChild;
-    newImage.style.visibility = "visible";
+// function showImage() {
+//     var newImage = document.getElementById('image-show').lastElementChild;
+//     newImage.style.visibility = "visible";
     
-    document.getElementById('image-upload').style.visibility = 'hidden';
+//     document.getElementById('image-upload').style.visibility = 'hidden';
 
-    document.getElementById('fileName').textContent = null;     // 기존 파일 이름 지우기
-}
+//     document.getElementById('fileName').textContent = null;     // 기존 파일 이름 지우기
+// }
 
-function loadFile(input) {
+// function loadFile(input) {
+//     var file = input.files[0];
+
+
+//     var newImage = new Image(); // 새 이미지 객체 생성
+//     newImage.onload = function() {
+//         var container = document.getElementById('image-show');
+//         container.innerHTML = ''; // 기존 이미지 삭제
+//         container.appendChild(newImage); // 새 이미지 추가
+//     };
+//     newImage.src = URL.createObjectURL(file);
+//     newImage.style.width = "70%";
+//     newImage.style.height = "70%";
+//     newImage.style.objectFit = "contain";
+// }
+
+function loadFile(input, imageShowId) {
     var file = input.files[0];
-
-
     var newImage = new Image(); // 새 이미지 객체 생성
     newImage.onload = function() {
-        var container = document.getElementById('image-show');
+        var container = document.getElementById(imageShowId);
         container.innerHTML = ''; // 기존 이미지 삭제
         container.appendChild(newImage); // 새 이미지 추가
     };
