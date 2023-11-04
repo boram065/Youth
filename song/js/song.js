44e4424dd323b68b210a5266c588fa58ccae30b9
boram065 <boram4784@gmail.com>
2023-11-04 14:37:12 +0900

    mp3 추가, js 노래 데이터 추가

--- song/js/song.js
+++ song/js/song.js
@@ -2,180 +2,568 @@ var song = [
     {
        "id": 1,
        "title": "♬ 송골매 - 어쩌다 마주친 그대 ♬",
-       "song": '/song/songMp3/송골매-어쩌다 마주친 그대(1982).mp3'
+       "song": '/song/songMp3/송골매-어쩌다 마주친 그대(1982).mp3',
+       "year": 1982
     },
 
     {
        "id": 2,
        "title": "♬ 윤수일 - 아파트 ♬",
-       "song": '/song/songMp3/윤수일-아파트(1982).mp3'
+       "song": '/song/songMp3/윤수일-아파트(1982).mp3',
+       "year": 1982
     },
 
     {
         "id": 3,
         "title": "♬ 김수철 - 젊은 그대 ♬",
-        "song": '/song/songMp3/김수철-젊은 그대(1984).mp3'
+        "song": '/song/songMp3/김수철-젊은 그대(1984).mp3',
+        "year": 1984
     },
 
     {
         "id": 4,
         "title": "♬ 나미 - 빙글빙글 ♬",
-        "song": '/song/songMp3/나미-빙글빙글(1984).mp3'
+        "song": '/song/songMp3/나미-빙글빙글(1984).mp3',
+        "year": 1984
     },
 
     {
         "id": 5,
         "title": "♬ 높은음자리 - 바다에 누워 ♬",
-        "song": '/song/songMp3/높은음자리-바다에 누워(1985).mp3'
+        "song": '/song/songMp3/높은음자리-바다에 누워(1985).mp3',
+        "year": 1985
     },
 
     {
         "id": 6,
         "title": "♬ 이선희 - 아 옛날이여 ♬",
-        "song": '/song/songMp3/이선희-아 옛날이여(1985).mp3'
+        "song": '/song/songMp3/이선희-아 옛날이여(1985).mp3',
+        "year": 1985
     },
 
     {
         "id": 7,
         "title": "♬ 김광석 - 이등병의 편지 ♬",
-        "song": '/song/songMp3/김광석-이등병의 편지(1986).mp3'
+        "song": '/song/songMp3/김광석-이등병의 편지(1986).mp3',
+        "year": 1986
     },
 
     {
         "id": 8,
         "title": "♬ 다섯손가락 - 풍선 ♬",
-        "song": '/song/songMp3/다섯손가락-풍선(1986).mp3'
+        "song": '/song/songMp3/다섯손가락-풍선(1986).mp3',
+        "year": 1986
     },
 
     {
         "id": 9,
         "title": "♬ 소방차 - 어젯밤 이야기 ♬",
-        "song": '/song/songMp3/소방차-어젯밤 이야기(1987).mp3'
+        "song": '/song/songMp3/소방차-어젯밤 이야기(1987).mp3',
+        "year": 1987
     },
 
     {
         "id": 10,
         "title": "♬ 이문세 - 깊은 밤을 날아서 ♬",
-        "song": '/song/songMp3/이문세-깊은 밤을 날아서(1987).mp3'
+        "song": '/song/songMp3/이문세-깊은 밤을 날아서(1987).mp3',
+        "year": 1987
     },
 
     {
         "id": 11,
         "title": "♬ 박남정 - 널 그리며 ♬",
-        "song": '/song/songMp3/박남정-널 그리며(1988).mp3'
+        "song": '/song/songMp3/박남정-널 그리며(1988).mp3',
+        "year": 1988
     },
 
     {
         "id": 12,
         "title": "♬ 이문세 - 가로수 그늘 아래서면 ♬",
-        "song": '/song/songMp3/이문세-가로수 그늘 아래서면(1988).mp3'
+        "song": '/song/songMp3/이문세-가로수 그늘 아래서면(1988).mp3',
+        "year": 1988
     },
 
     {
         "id": 13,
         "title": "♬ 이문세 - 붉은 노을 ♬",
-        "song": '/song/songMp3/이문세-붉은 노을(1988).mp3'
+        "song": '/song/songMp3/이문세-붉은 노을(1988).mp3',
+        "year": 1988
     },
 
     {
         "id": 14,
         "title": "♬ 변진섭 - 희망사항 ♬",
-        "song": '/song/songMp3/변진섭-희망사항(1989).mp3'
+        "song": '/song/songMp3/변진섭-희망사항(1989).mp3',
+        "year": 1989
     },
 
     {
         "id": 15,
         "title": "♬ 이상은 - 담다디 ♬",
-        "song": '/song/songMp3/이상은-담다디(1989).mp3'
+        "song": '/song/songMp3/이상은-담다디(1989).mp3',
+        "year": 1989
     },
 
     {
         "id": 16,
         "title": "♬ 신해철 - 그대에게 ♬",
-        "song": '/song/songMp3/신해철-그대에게(1991).mp3'
+        "song": '/song/songMp3/신해철-그대에게(1991).mp3',
+        "year": 1991
     },
 
     {
         "id": 17,
         "title": "♬ 김광석 - 잊어야 한다는 마음으로 ♬",
-        "song": '/song/songMp3/김광석-잊어야 한다는 마음으로(1992).mp3'
+        "song": '/song/songMp3/김광석-잊어야 한다는 마음으로(1992).mp3',
+        "year": 1992
     },
 
     {
         "id": 18,
         "title": "♬ 김광석 - 서른 즈음에 ♬",
-        "song": '/song/songMp3/김광석-서른 즈음에(1994).mp3'
+        "song": '/song/songMp3/김광석-서른 즈음에(1994).mp3',
+        "year": 1994
     },
 
     {
         "id": 19,
-        "title": "♬ 터보 - 검은 고양이 ♬",
-        "song": '/song/songMp3/터보-검은 고양이(1995).mp3'
+        "title": "♬ 터보 - 검은 고양이 네로 ♬",
+        "song": '/song/songMp3/터보-검은 고양이 네로(1995).mp3',
+        "year": 1995
     },
 
     {
         "id": 20,
         "title": "♬ 자자 - 버스 안에서 ♬",
-        "song": '/song/songMp3/자자-버스 안에서(1996).mp3'
+        "song": '/song/songMp3/자자-버스 안에서(1996).mp3',
+        "year": 1996
     },
 
     {
         "id": 21,
         "title": "♬ 터보 - Twist King ♬",
-        "song": '/song/songMp3/터보-Twist King(1996).mp3'
+        "song": '/song/songMp3/터보-Twist King(1996).mp3',
+        "year": 1996
     },
 
     {
         "id": 22,
         "title": "♬ SES - I'm Your Girl♬ ",
-        "song": "/song/songMp3/SES-I'm Your Girl(1997).mp3"
+        "song": "/song/songMp3/SES-I'm Your Girl(1997).mp3",
+        "year": 1997
     },
 
     {
         "id": 23,
         "title": "♬ 젝스키스 - 커플 ♬",
-        "song": "/song/songMp3/젝스키스-커플(1998).mp3"
+        "song": "/song/songMp3/젝스키스-커플(1998).mp3",
+        "year": 1998
     },
 
     {
         "id": 24,
         "title": "♬ SES - Dreams Come True ♬",
-        "song": "/song/songMp3/SES-Dreams Come True(1998).mp3"
+        "song": "/song/songMp3/SES-Dreams Come True(1998).mp3",
+        "year": 1998
     },
 
     {
         "id": 25,
         "title": "♬ 임창정 - 바람과 함께 사라지다 ♬",
-        "song": "/song/songMp3/임창정-바람과 함께 사라지다(1999).mp3"
+        "song": "/song/songMp3/임창정-바람과 함께 사라지다(1999).mp3",
+        "year": 1999
     },
 
     {
         "id": 26,
         "title": "♬ 젝스키스 - 폼생폼사 ♬",
-        "song": "/song/songMp3/젝스키스-폼생폼사(1999).mp3"
+        "song": "/song/songMp3/젝스키스-폼생폼사(1999).mp3",
+        "year": 1999
     },
 
     {
         "id": 27,
         "title": "♬ god - 사랑해 그리고 기억해 ♬",
-        "song": "/song/songMp3/god-사랑해 그리고 기억해(1999).mp3"
+        "song": "/song/songMp3/god-사랑해 그리고 기억해(1999).mp3",
+        "year": 1999
     },
 
     {
         "id": 28,
         "title": "♬ god - 어머님께 ♬",
-        "song": "/song/songMp3/god-어머님께(1999).mp3"
+        "song": "/song/songMp3/god-어머님께(1999).mp3",
+        "year": 1999
     },
 
     {
         "id": 29,
         "title": "♬ 마로니에 - 칵테일 사랑 ♬",
-        "song": "/song/songMp3/마로니에-칵테일 사랑(2001).mp3"
+        "song": "/song/songMp3/마로니에-칵테일 사랑(2001).mp3",
+        "year": 2001
     },
 
     {
         "id": 30,
         "title": "♬ 부활 - Naver Ending Story ♬",
-        "song": "/song/songMp3/부활-Naver Ending Story(2002).mp3"
+        "song": "/song/songMp3/부활-Naver Ending Story(2002).mp3",
+        "year": 2002
     },
+
+    {
+        "id": 31,
+        "title": "♬ 강수지 - 보라빛 향기 ♬",
+        "song": "/song/songMp3/강수지-보라빛 향기(1990).mp3",
+        "year": 1990
+    },
+
+    {
+        "id": 32,
+        "title": "♬ 어우러기 - 밤에 피는 장미 ♬",
+        "song": "/song/songMp3/어우러기-밤에 피는 장미(1985).mp3",
+        "year": 1985
+    },
+
+    {
+        "id": 33,
+        "title": "♬ 이문세 - 가을이 오면 ♬",
+        "song": "/song/songMp3/이문세-가을이 오면(1987).mp3",
+        "year": 1987
+    },
+
+    {
+        "id": 34,
+        "title": "♬ 이문세 - 소녀 ♬",
+        "song": "/song/songMp3/이문세-소녀(1985).mp3",
+        "year": 1985
+    },
+
+    {
+        "id": 35,
+        "title": "♬ 이범학 - 이별 아닌 이별 ♬",
+        "song": "/song/songMp3/이범학-이별 아닌 이별(1991).mp3",
+        "year": 1991
+    },
+
+    {
+        "id": 36,
+        "title": "♬ 서태지와 아이들 - 난 알아요 ♬",
+        "song": "/song/songMp3/서태지와 아이들-난 알아요(1992).mp3",
+        "year": 1992
+    },
+
+    {
+        "id": 37,
+        "title": "♬ 서태지와 아이들 - 환상 속의 그대 ♬",
+        "song": '/song/songMp3/서태지와 아이들-환상 속의 그대(1992).mp3',
+        "year": 1992
+     },
+ 
+     {
+        "id": 38,
+        "title": "♬ 김원준 - 모두 잠든 후에 ♬",
+        "song": '/song/songMp3/김원준-모두 잠든 후에(1993).mp3',
+        "year": 1993
+     },
+ 
+     {
+         "id": 39,
+         "title": "♬ 현진영 - 흐린 기억속의 그대 ♬",
+         "song": '/song/songMp3/현진영-흐린 기억속의 그대(1993).mp3',
+         "year": 1993
+     },
+ 
+     {
+         "id": 40,
+         "title": "♬ 투투 - 일과 이분의 일 ♬",
+         "song": '/song/songMp3/투투-일과 이분의 일(1994).mp3',
+         "year": 1994
+     },
+ 
+     {
+         "id": 41,
+         "title": "♬ 김원준-너 없는 동안 ♬",
+         "song": '/song/songMp3/김원준-너 없는 동안(1994).mp3',
+         "year": 1994
+     },
+ 
+     {
+         "id": 42,
+         "title": "♬ 서태지와 아이들 - Come Back Home ♬",
+         "song": '/song/songMp3/서태지와 아이들-Come Back Home(1995).mp3',
+         "year": 1995
+     },
+ 
+     {
+         "id": 43,
+         "title": "♬ 룰라 - 날개 잃은 천사 ♬",
+         "song": '/song/songMp3/룰라-날개 잃은 천사(1995).mp3',
+         "year": 1995
+     },
+ 
+     {
+         "id": 44,
+         "title": "♬ 박미경 - 이브의 경고♬",
+         "song": '/song/songMp3/박미경-이브의 경고(1995).mp3',
+         "year": 1995
+     },
+ 
+     {
+         "id": 45,
+         "title": "♬ 육각수 - 흥보가 기가 막혀 ♬",
+         "song": '/song/songMp3/육각수-흥보가 기가 막혀(1995).mp3',
+         "year": 1995
+     },
+ 
+     {
+         "id": 46,
+         "title": "♬ 터보 - 나 어릴적 꿈 ♬",
+         "song": '/song/songMp3/터보-나 어릴적 꿈(1995).mp3',
+         "year": 1995
+     },
+ 
+     {
+         "id": 47,
+         "title": "♬ 뱅크 - 가질 수 없는 너 ♬",
+         "song": '/song/songMp3/뱅크-가질 수 없는 너(1995).mp3',
+         "year": 1995
+     },
+ 
+     {
+         "id": 48,
+         "title": "♬ 김건모 - 잘못된 만남 ♬",
+         "song": '/song/songMp3/김건모-잘못된 만남(1995).mp3',
+         "year": 1995
+     },
+ 
+     {
+         "id": 49,
+         "title": "♬ DJ.DOC - 여름 이야기 ♬",
+         "song": '/song/songMp3/DJ.DOC-여름 이야기(1996).mp3',
+         "year": 1996
+     },
+ 
+     {
+         "id": 50,
+         "title": "♬ 김정민 - 슬픈 언약식 ♬",
+         "song": '/song/songMp3/김정민-슬픈 언약식(1996).mp3',
+         "year": 1996
+     },
+ 
+     {
+         "id": 51,
+         "title": "♬ 솔리드 - 천생연분 ♬",
+         "song": '/song/songMp3/솔리드-천생연분(1996).mp3',
+         "year": 1996
+     },
+ 
+     {
+         "id": 52,
+         "title": "♬ 클론 - 쿵따리 샤바라 ♬",
+         "song": '/song/songMp3/클론-쿵따리 샤바라(1996).mp3',
+         "year": 1996
+     },
+ 
+     {
+         "id": 53,
+         "title": "♬ 영턱스클럽 - 정 ♬",
+         "song": '/song/songMp3/영턱스클럽-정(1996).mp3',
+         "year": 1996
+     },
+ 
+     {
+         "id": 54,
+         "title": "♬ 룰라 - 3! 4! ♬",
+         "song": '/song/songMp3/룰라-3! 4!(1996).mp3',
+         "year": 1996
+     },
+ 
+     {
+         "id": 55,
+         "title": "♬ 김원준 - Show ♬",
+         "song": '/song/songMp3/김원준-Show(1996).mp3',
+         "year": 1996
+     },
+ 
+     {
+         "id": 56,
+         "title": "♬ 쿨 - 해변의 여인 ♬",
+         "song": '/song/songMp3/쿨-해변의 여인(1997).mp3',
+         "year": 1997
+     },
+ 
+     {
+         "id": 57,
+         "title": "♬ 룰라 - 연인 ♬",
+         "song": '/song/songMp3/룰라-연인(1997).mp3',
+         "year": 1997
+     },
+ 
+     {
+         "id": 58,
+         "title": "♬ UP - 뿌요뿌요 ♬ ",
+         "song": "/song/songMp3/UP-뿌요뿌요(1997).mp3",
+         "year": 1997
+     },
+ 
+     {
+         "id": 59,
+         "title": "♬ DJ.DOC - DOC와 춤을 ♬",
+         "song": "/song/songMp3/DJ.DOC-DOC와 춤을(1997).mp3",
+         "year": 1997
+     },
+ 
+     {
+         "id": 60,
+         "title": "♬ HOT - 캔디 ♬",
+         "song": "/song/songMp3/HOT-캔디(1997).mp3",
+         "year": 1997
+     },
+ 
+     {
+         "id": 61,
+         "title": "♬ 터보 - Goodbye YesterDay ♬",
+         "song": "/song/songMp3/터보-Goodbye YesterDay(1997).mp3",
+         "year": 1997
+     },
+ 
+     {
+         "id": 62,
+         "title": "♬ 김장훈 - 나와 같다면 ♬",
+         "song": "/song/songMp3/김장훈-나와 같다면(1998).mp3",
+         "year": 1998
+     },
+ 
+     {
+         "id": 63,
+         "title": "♬ 쿨 - 애상 ♬",
+         "song": "/song/songMp3/쿨-애상(1998).mp3",
+         "year": 1998
+     },
+ 
+     {
+         "id": 64,
+         "title": "♬ 조성모 - To Heaven ♬",
+         "song": "/song/songMp3/조성모-To Heaven(1998).mp3",
+         "year": 1998
+     },
+ 
+     {
+         "id": 65,
+         "title": "♬ 한스밴드 - 오락실 ♬",
+         "song": "/song/songMp3/한스밴드-오락실(1998).mp3",
+         "year": 1998
+     },
+ 
+     {
+         "id": 66,
+         "title": "♬ 핑클 - 내 남자친구에게 ♬",
+         "song": "/song/songMp3/핑클-내 남자친구에게(1999).mp3",
+         "year": 1999
+     },
+ 
+     {
+         "id": 67,
+         "title": "♬ 핑클 - 영원한 사랑 ♬",
+         "song": "/song/songMp3/핑클-영원한 사랑(1999).mp3",
+         "year": 1999
+     },
+ 
+     {
+         "id": 68,
+         "title": "♬ 이정현 - 와 ♬",
+         "song": "/song/songMp3/이정현-와(1999).mp3",
+         "year": 1999
+     },
+ 
+     {
+         "id": 69,
+         "title": "♬ god - 하늘색 풍선 ♬",
+         "song": "/song/songMp3/god-하늘색 풍선(2000).mp3",
+         "year": 2000
+     },
+ 
+     {
+         "id": 70,
+         "title": "♬ god - 촛불하나 ♬",
+         "song": "/song/songMp3/god-촛불하나(2000).mp3",
+         "year": 2000
+     },
+ 
+     {
+         "id": 71,
+         "title": "♬ 자전거 탄 풍경 - 너에게 난, 나에게 넌 ♬",
+         "song": "/song/songMp3/자전거 탄 풍경-너에게 난, 나에게 넌(2001).mp3",
+         "year": 2001
+     },
+ 
+     {
+         "id": 72,
+         "title": "♬ 더 자두 - 김밥 ♬",
+         "song": "/song/songMp3/더 자두-김밥(2003).mp3",
+         "year": 2003
+     },
+
+     {
+        "id": 73,
+        "title": "♬ 김장훈 - 고속도로 로망스 ♬",
+        "song": "/song/songMp3/김장훈-고속도로 로망스(2004).mp3",
+        "year": 2004
+    },
+
+    {
+        "id": 74,
+        "title": "♬ 거북이 - 빙고 ♬",
+        "song": "/song/songMp3/거북이-빙고(2004).mp3",
+        "year": 2004
+    },
+
+    {
+        "id": 75,
+        "title": "♬ 거북이 - 비행기 ♬",
+        "song": "/song/songMp3/거북이-비행기(2006).mp3",
+        "year": 2006
+    },
+
+    {
+        "id": 76,
+        "title": "♬ 김장훈 - 허니 ♬",
+        "song": "/song/songMp3/김장훈-허니(2006).mp3",
+        "year": 2006
+    },
+
+    {
+        "id": 78,
+        "title": "♬ 김광석 - 바람이 불어오는 곳 ♬",
+        "song": "/song/songMp3/김광석-바람이 불어오는 곳(2006).mp3",
+        "year": 2006
+    },
+
+    {
+        "id": 79,
+        "title": "♬ 김광석 - 두바퀴로 가는 자동차 ♬",
+        "song": "/song/songMp3/김광석-두바퀴로 가는 자동차(2006).mp3",
+        "year": 2006
+    },
+
+    {
+        "id": 80,
+        "title": "♬ 김아중 - Maria ♬",
+        "song": "/song/songMp3/김아중-Maria(2006).mp3",
+        "year": 2006
+    },
+
+    {
+        "id": 81,
+        "title": "♬ 하하 - 키 작은 꼬마이야기 ♬",
+        "song": "/song/songMp3/하하-키 작은 꼬마이야기(2007).mp3",
+        "year": 2007
+    },
+
+    {
+        "id": 82,
+        "title": "♬ 김현성 - Heaven ♬",
+        "song": "/song/songMp3/김현성-Heaven(2002).mp3",
+        "year": 2002
+    }
+
 ];
