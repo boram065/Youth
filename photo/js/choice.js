d42f352bf01465457d21e9df35edce0d5e92d96e
boram065 <boram4784@gmail.com>
2023-11-08 20:21:11 +0900

    choice js 더 깔끔하게 코드 수정

--- photo/js/choice.js
+++ photo/js/choice.js
@@ -1,13 +1,12 @@
+var screen1 = document.querySelector(".screen1");
+var screen2 = document.querySelector(".screen2");
+
 function nextFrame() {
-    var screen1 = document.querySelector(".screen1");
-    var screen2 = document.querySelector(".screen2");
     screen1.style.display = "none";
     screen2.style.display = "block";
 }
 
 function preFrame() {
-    var screen1 = document.querySelector(".screen1");
-    var screen2 = document.querySelector(".screen2");
-    screen1.style.display = "block";
-    screen2.style.display = "none";
+    screen1.style.display = "none";
+    screen2.style.display = "block";
 }
